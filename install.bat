f0369529d50298fa8973c3c661340b8bcc2809cb
Bart de Boer <bartdb@gmail.com>
2013-06-07 12:50:59 +0200

    Added install/uninstall bat

---
+++ install.bat
@@ -0,0 +1,6 @@
+%0\ 
+cd %0\.. 
+cd /d %0\.. 
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil Logging3DproxyApp.exe 
+net start Proxy3DService
+pause
---
+++ uninstall.bat
@@ -0,0 +1,6 @@
+%0\ 
+cd %0\.. 
+cd /d %0\.. 
+net stop Proxy3DService
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil /u Logging3DproxyApp.exe 
+pause
